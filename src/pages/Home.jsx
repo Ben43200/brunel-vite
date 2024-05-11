aa530ce8b3e651661c41b38199460732316792c2
meteoben <meteoben@gmail.com>
2024-05-11 14:26:19 +0200

    réglage pb layout footer en sumpprimer la règle width 1240px;

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ const Home = () => {
 return(
     <main>
     <p className="sub-title">Titre</p>
-    <img src= {new URL('../assets/photo1.webp', import.meta.url).href} alt="photo1" />
+    {/* <img src= {new URL('../assets/photo1.webp', import.meta.url).href} alt="photo1" /> */}
     </main>
 )
 }
