acdb5f7a68f7258743e56083334d7ad0f993197b
meteoben <meteoben@gmail.com>
2024-05-13 06:42:57 +0200

    csp désactivée ajout photo 1 pb position mobile

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import "./home.scss"
 const Home = () => {
 return(
-    <main>
+    <main className="home-container">
     {/* <img src= {new URL('../assets/photo1.webp', import.meta.url).href} alt="photo1" /> */}
     </main>
 )
