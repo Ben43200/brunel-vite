73eb105e5d7010bed2b33ef820d8465ebe0f3434
meteoben <meteoben@gmail.com>
2024-05-13 07:35:27 +0200

    cercle mobile ok media image en granbd

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,9 @@ import "./home.scss"
 const Home = () => {
 return(
     <main className="home-container">
+        <div className="circular--landscape">
+        {/* <img src="/images/accueil1-mobile.webp" className="mobile-circle" /> */}
+        </div>
     {/* <img src= {new URL('../assets/photo1.webp', import.meta.url).href} alt="photo1" /> */}
     </main>
 )
