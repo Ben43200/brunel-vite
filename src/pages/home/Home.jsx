bb52f4e514c2a529d94c173749337aed6a8557eb
meteoben <meteoben@gmail.com>
2024-05-16 12:08:01 +0200

    on est bon pour le grand carouse essail

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,36 @@
 import "./home.scss"
 import Carousel from "../../components/carousel/Carousel.jsx";
-// import FullCarousel from "../../components/carousel/FullCarousel.jsx";
+import FullCarousel from "../../components/carousel/FullCarousel.jsx";
 const Home = () => {
     // const slides = ["Dehli", "Mumbai", "Pune"];
     const slides = ["circular--landscape-1", "circular--landscape-2", "circular--landscape-3"]
+   
+    const ImageData = [
+    
+        {
+          image:
+            "/public/images/accueil1.webp",
+        },
+        {
+          image:
+            "/public/images/accueil2.webp",
+        },
+        {
+          image:
+            "/public/images/accueil3.webp",
+        },
+      ];  
+   
+   
     // const urlsImg = ["/public/accueil1.webp", "/public/accueil2.webp", "/public/accueil3.webp"]
- return(
+ 
+ 
+    return(
     <main className="home-container">
         {/* <div className="circular--landscape"> */}
          <Carousel  slides={slides}   />
-         {/* <FullCarousel urlsImg={urlsImg} /> */}
+         <FullCarousel bigSlides={ImageData} />
+         {/* <FullCarousel  /> */}
         {/* <Carousel  slides={slides} className={className} />  */}
         {/* <Carousel  className={className} /> */}
 <div>Texte
