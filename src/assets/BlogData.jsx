518ccf2b566c743955064a90aaa9c4d153cb532d
meteoben <meteoben@gmail.com>
2024-05-16 18:03:21 +0200

    commit intermédiaire blog encore quelques pb de calage

--- src/assets/BlogData.jsx
+++ src/assets/BlogData.jsx
@@ -9,58 +9,50 @@ export const BLOGDATAS= [
 	{
 		name: "Qu’est-ce que la micronutrition ?",
 		image : "images/micronutrition.webp",
-		alt : "image omega",
-		text : "On en parle beaucoup, mais que sont-ils exactement ? A quoi servent-ils ? Où les trouve-t-on ?" +
-		"Les omégas 3 sont des acides gras à longues chaines avec plusieurs doubles liaisons. Ce sont..."
+		alt : "image micronutrition",
+		text : "La Micronutrition, une approche de la nutrition médicalisée adaptée à chacun…La Micronutrition consiste à satisfaire les besoins en micronutriments de l’individu, par une alimentation..."
 		},
 		{
-			name: "Que sont les omégas 3?",
+			name: "Quand faire appel à la micronutrition ?",
 			image : "images/appel-micronutrition.webp",
-			alt : "image omega",
-			text : "On en parle beaucoup, mais que sont-ils exactement ? A quoi servent-ils ? Où les trouve-t-on ?" +
-			"Les omégas 3 sont des acides gras à longues chaines avec plusieurs doubles liaisons. Ce sont..."
+			alt : "image appel micronutrition",
+			text : "Parce qu’elle n’est pas réservée à une population particulière mais au contraire adaptée pour apporter des solutions personnalisées à chaque période de la vie, la micronutrition s’adresse à tous ceux..."
 			},
 			{
-				name: "Que sont les omégas 3?",
+				name: "Qu’est-ce que la dysbiose ?",
 				image : "images/omega.webp",
 				alt : "image omega",
-				text : "On en parle beaucoup, mais que sont-ils exactement ? A quoi servent-ils ? Où les trouve-t-on ?" +
-				"Les omégas 3 sont des acides gras à longues chaines avec plusieurs doubles liaisons. Ce sont..."
+				text : "La dysbiose (par opposition à l’eubiose) est le nom donné à « la guerre dans l’intestin ». Les bactéries « méchantes » et autres microorganismes (tels que levures, parasites) ont pris le dessus sur..."
 				},
 				{
-					name: "Que sont les omégas 3?",
-					image : "images/omega.webp",
-					alt : "image omega",
-					text : "On en parle beaucoup, mais que sont-ils exactement ? A quoi servent-ils ? Où les trouve-t-on ?" +
-					"Les omégas 3 sont des acides gras à longues chaines avec plusieurs doubles liaisons. Ce sont..."
+					name: "Comment revenir à la paix dans l’intestin ?",
+					image : "images/paix-intestin.webp",
+					alt : "image paix intestin",
+					text : "Lorsque la dysbiose est installée, le changement d’alimentation, l’exclusion d’aliments créant une hypersensibilité ne seront, le plus souvent, pas suffisant pour rétablir la paix. Il faut réensemencer..."
 					},
 					{
-						name: "Que sont les omégas 3?",
+						name: "Qu’est-ce que l’hyperperméabilité intestinale ?",
 						image : "images/omega.webp",
-						alt : "image omega",
-						text : "On en parle beaucoup, mais que sont-ils exactement ? A quoi servent-ils ? Où les trouve-t-on ?" +
-						"Les omégas 3 sont des acides gras à longues chaines avec plusieurs doubles liaisons. Ce sont..."
+						alt : "omega",
+						text : "L’hyperperabilité intestinale représente l’ouverture des jonctions serrées qui se situent entre chaque cellule de l’intestin. Ces jonctions, qui peuvent être représentées comme des portes..."
 						},
 						{
-							name: "Que sont les omégas 3?",
-							image : "images/omega.webp",
-							alt : "image omega",
-							text : "On en parle beaucoup, mais que sont-ils exactement ? A quoi servent-ils ? Où les trouve-t-on ?" +
-							"Les omégas 3 sont des acides gras à longues chaines avec plusieurs doubles liaisons. Ce sont..."
+							name: "Que se passe-t-il quand on manque de certains micronutriments ?",
+							image : "images/manque-micronutriments.webp",
+							alt : "legumineuse",
+							text : "L’organisme fonctionne alors en mode « économie » en dépensant un minimum d’énergie et en stockant les moindres excès. C’est souvent le cas après des..."
 							},
 							{
-								name: "Que sont les omégas 3?",
-								image : "images/omega.webp",
-								alt : "image omega",
-								text : "On en parle beaucoup, mais que sont-ils exactement ? A quoi servent-ils ? Où les trouve-t-on ?" +
-								"Les omégas 3 sont des acides gras à longues chaines avec plusieurs doubles liaisons. Ce sont..."
+								name: "Au petit-déjeuner on nourrit les neurones",
+								image : "images/petit-dej.webp",
+								alt : "image oeuf petit dej",
+								text : "Objectif du premier repas de la journée : fournir l’énergie nécessaire pour tenir toute la matinée de travail et permettre à l’influx nerveux de se propager le long de nos..."
 								},
 								{
-									name: "Que sont les omégas 3?",
-									image : "images/omega.webp",
-									alt : "image omega",
-									text : "On en parle beaucoup, mais que sont-ils exactement ? A quoi servent-ils ? Où les trouve-t-on ?" +
-									"Les omégas 3 sont des acides gras à longues chaines avec plusieurs doubles liaisons. Ce sont..."
+									name: "Huiles toxiques ",
+									image : "images/huiles-toxiques.webp",
+									alt : "image huiles toxiques",
+									text : "Un article du Canard enchainé * nous en apprend de belles sur les huiles végétales.On se doutait bien que les processus de transformation industrielle des huiles..."
 									},
 									
 
