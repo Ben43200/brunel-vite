802a811aad3a72b7838997a1dc11db00663ae1ec
meteoben <meteoben@gmail.com>
2024-05-16 10:03:39 +0200

    mise en commentaire fullcarousel

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,15 @@
 import "./home.scss"
 import Carousel from "../../components/carousel/Carousel.jsx";
-import FullCarousel from "../../components/carousel/FullCarousel.jsx";
+// import FullCarousel from "../../components/carousel/FullCarousel.jsx";
 const Home = () => {
     // const slides = ["Dehli", "Mumbai", "Pune"];
     const slides = ["circular--landscape-1", "circular--landscape-2", "circular--landscape-3"]
-    const urlsImg = ["/public/accueil1.webp", "/public/accueil2.webp", "/public/accueil3.webp"]
+    // const urlsImg = ["/public/accueil1.webp", "/public/accueil2.webp", "/public/accueil3.webp"]
  return(
     <main className="home-container">
         {/* <div className="circular--landscape"> */}
          <Carousel  slides={slides}   />
-         <FullCarousel urlsImg={urlsImg} />
+         {/* <FullCarousel urlsImg={urlsImg} /> */}
         {/* <Carousel  slides={slides} className={className} />  */}
         {/* <Carousel  className={className} /> */}
 <div>Texte
