d7820f6b580ee1a95a8a75f4bf6bb90fc7706102
meteoben <meteoben@gmail.com>
2024-05-20 02:14:49 +0200

    ajout complete Text blogdatas

--- src/assets/BlogData.jsx
+++ src/assets/BlogData.jsx
@@ -1,69 +1,76 @@
-export const BLOGDATAS= [
-    {
-		"id": 0,
-    "name": "Que sont les omégas 3?",
-    "image" : "images/omega.webp",
-    "alt" : "image omega",
-    "text" : "On en parle beaucoup, mais que sont-ils exactement ? A quoi servent-ils ? Où les trouve-t-on ?" +
-	"Les omégas 3 sont des acides gras à longues chaines avec plusieurs doubles liaisons. Ce sont...",
-	},
-	{
-		"id": 1,
-		"name": "Qu’est-ce que la micronutrition ?",
-		"image" : "images/micronutrition.webp",
-		"alt" : "image micronutrition",
-		"text" : "La Micronutrition, une approche de la nutrition médicalisée adaptée à chacun…La Micronutrition consiste à satisfaire les besoins en micronutriments de l’individu, par une alimentation...",
-	},
-		{
-			"id": 2,
-			"name": "Quand faire appel à la micronutrition ?",
-			"image" : "images/appel-micronutrition.webp",
-			"alt" : "image appel micronutrition",
-			"text" : "Parce qu’elle n’est pas réservée à une population particulière mais au contraire adaptée pour apporter des solutions personnalisées à chaque période de la vie, la micronutrition s’adresse à tous ceux...",
-			},
-			{
-				"id": 3,
-				"name": "Qu’est-ce que la dysbiose ?",
-				"image" : "images/omega.webp",
-				"alt" : "image omega",
-				"text" : "La dysbiose (par opposition à l’eubiose) est le nom donné à « la guerre dans l’intestin ». Les bactéries « méchantes » et autres microorganismes (tels que levures, parasites) ont pris le dessus sur...",
-				},
-				{
-					"id": 4,
-					"name": "Comment revenir à la paix dans l’intestin ?",
-					"image" : "images/paix-intestin.webp",
-					"alt" : "image paix intestin",
-					"text" : "Lorsque la dysbiose est installée, le changement d’alimentation, l’exclusion d’aliments créant une hypersensibilité ne seront, le plus souvent, pas suffisant pour rétablir la paix. Il faut réensemencer...",
-					},
-					{
-						"id": 5,
-						"name": "Qu’est-ce que l’hyperperméabilité intestinale ?",
-						"image" : "images/omega.webp",
-						"alt" : "omega",
-						"text" : "L’hyperperabilité intestinale représente l’ouverture des jonctions serrées qui se situent entre chaque cellule de l’intestin. Ces jonctions, qui peuvent être représentées comme des portes...",
-						},
-						{
-							"id": 6,
-							"name": "Que se passe-t-il quand on manque de certains micronutriments ?",
-							"image" : "images/manque-micronutriments.webp",
-							"alt" : "legumineuse",
-							"text" : "L’organisme fonctionne alors en mode « économie » en dépensant un minimum d’énergie et en stockant les moindres excès. C’est souvent le cas après des...",
-							},
-							{
-								"id": 7,
-								"name": "Au petit-déjeuner on nourrit les neurones",
-								"image" : "images/petit-dej.webp",
-								"alt" : "image oeuf petit dej",
-								"text" : "Objectif du premier repas de la journée : fournir l’énergie nécessaire pour tenir toute la matinée de travail et permettre à l’influx nerveux de se propager le long de nos...",
-								},
-								{
-									"id": 8,
-									"name": "Huiles toxiques ",
-									"image" : "images/huiles-toxiques.webp",
-									"alt" : "image huiles toxiques",
-									"text" : "Un article du Canard enchainé * nous en apprend de belles sur les huiles végétales.On se doutait bien que les processus de transformation industrielle des huiles...",
-									},
-									
-
-
-]
+export const BLOGDATAS = [
+  {
+    id: 0,
+    name: "Que sont les omégas 3?",
+    image: "images/omega.webp",
+    alt: "image omega",
+    text:
+      "On en parle beaucoup, mais que sont-ils exactement ? A quoi servent-ils ? Où les trouve-t-on ?" +
+      "Les omégas 3 sont des acides gras à longues chaines avec plusieurs doubles liaisons. Ce sont...",
+    completText: ["text1", "text2"],
+  },
+  {
+    id: 1,
+    name: "Qu’est-ce que la micronutrition ?",
+    image: "images/micronutrition.webp",
+    alt: "image micronutrition",
+    text: "La Micronutrition, une approche de la nutrition médicalisée adaptée à chacun…La Micronutrition consiste à satisfaire les besoins en micronutriments de l’individu, par une alimentation...",
+    completText: ["text1", "text2"],
+  },
+  {
+    id: 2,
+    name: "Quand faire appel à la micronutrition ?",
+    image: "images/appel-micronutrition.webp",
+    alt: "image appel micronutrition",
+    text: "Parce qu’elle n’est pas réservée à une population particulière mais au contraire adaptée pour apporter des solutions personnalisées à chaque période de la vie, la micronutrition s’adresse à tous ceux...",
+    completText: ["text1", "text2"],
+  },
+  {
+    id: 3,
+    name: "Qu’est-ce que la dysbiose ?",
+    image: "images/omega.webp",
+    alt: "image omega",
+    text: "La dysbiose (par opposition à l’eubiose) est le nom donné à « la guerre dans l’intestin ». Les bactéries « méchantes » et autres microorganismes (tels que levures, parasites) ont pris le dessus sur...",
+    completText: ["text1", "text2"],
+  },
+  {
+    id: 4,
+    name: "Comment revenir à la paix dans l’intestin ?",
+    image: "images/paix-intestin.webp",
+    alt: "image paix intestin",
+    text: "Lorsque la dysbiose est installée, le changement d’alimentation, l’exclusion d’aliments créant une hypersensibilité ne seront, le plus souvent, pas suffisant pour rétablir la paix. Il faut réensemencer...",
+    completText: ["text1", "text2"],
+  },
+  {
+    id: 5,
+    name: "Qu’est-ce que l’hyperperméabilité intestinale ?",
+    image: "images/omega.webp",
+    alt: "omega",
+    text: "L’hyperperabilité intestinale représente l’ouverture des jonctions serrées qui se situent entre chaque cellule de l’intestin. Ces jonctions, qui peuvent être représentées comme des portes...",
+    completText: ["text1", "text2"],
+  },
+  {
+    id: 6,
+    name: "Que se passe-t-il quand on manque de certains micronutriments ?",
+    image: "images/manque-micronutriments.webp",
+    alt: "legumineuse",
+    text: "L’organisme fonctionne alors en mode « économie » en dépensant un minimum d’énergie et en stockant les moindres excès. C’est souvent le cas après des...",
+    completText: ["text1", "text2"],
+  },
+  {
+    id: 7,
+    name: "Au petit-déjeuner on nourrit les neurones",
+    image: "images/petit-dej.webp",
+    alt: "image oeuf petit dej",
+    text: "Objectif du premier repas de la journée : fournir l’énergie nécessaire pour tenir toute la matinée de travail et permettre à l’influx nerveux de se propager le long de nos...",
+    completText: ["text1", "text2"],
+  },
+  {
+    id: 8,
+    name: "Huiles toxiques ",
+    image: "images/huiles-toxiques.webp",
+    alt: "image huiles toxiques",
+    text: "Un article du Canard enchainé * nous en apprend de belles sur les huiles végétales.On se doutait bien que les processus de transformation industrielle des huiles...",
+    completText: ["text1", "text2"],
+  },
+];
