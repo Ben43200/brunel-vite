ab9ec962dcb44149fae0a73251ba3e6c50f288e4
meteoben <meteoben@gmail.com>
2024-05-21 06:24:05 +0200

    suppression commentaire blogitem

--- src/components/blogitem/BlogItem.jsx
+++ src/components/blogitem/BlogItem.jsx
@@ -5,19 +5,12 @@ import { BLOGDATAS } from "../../assets/BlogData.jsx";
 import { blogdataitem } from "../../assets/BlogDataItem.jsx";
 // import CompletText from "../CompletText.jsx";
 const BlogItem =(props) =>{
-    // console.log(completText)
+
    console.log(props)
     let {id} = useParams();
     console.log(id)
     const blogitem = BLOGDATAS.find(blogitem => String(blogitem.id) === id);
-    // const listItems = blogdataitem.map(blogitem => <li key={`${blogitem.id}`}>{blogitem}</li>)
-    // console.log(blogitem)
-    // console.log("test")
-    // const [currentId] = useOutletContext()
-    const nestedData = [
-        { id: 1, items: ['Apple', 'Banana'] },
-        { id: 2, items: ['Strawberry', 'Pineapple'] },
-      ];
+ 
     return(
     <main>
     {/* <img src={`/${blogitem.image}`} alt={blogitem.alt} />
