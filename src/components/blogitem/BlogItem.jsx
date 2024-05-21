0e9e8ffa50a961754e845f484db18206e45251ed
meteoben <meteoben@gmail.com>
2024-05-21 06:32:31 +0200

    implementation usenavigate blog item

--- src/components/blogitem/BlogItem.jsx
+++ src/components/blogitem/BlogItem.jsx
@@ -4,14 +4,51 @@ import { useParams } from "react-router-dom";
 import { BLOGDATAS } from "../../assets/BlogData.jsx";
 import { blogdataitem } from "../../assets/BlogDataItem.jsx";
 // import CompletText from "../CompletText.jsx";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+
+
 const BlogItem =(props) =>{
 
+    const navigate = useNavigate();
+    // const { id } = useParams();
+    // const [data, setData] = useState(null);
+  
+  //   useEffect(() => {
+  //     const foundData = MY_DATA.find((item) => item.id === id);
+  //     if (foundData) {
+  //       setData(foundData);
+  //     } else {
+  //       navigate('/error');
+  //     }
+  //   }, [id, navigate]);
+  
+  // //   return data ? <div>{data.content}</div> : null;
+  // // }
    console.log(props)
     let {id} = useParams();
     console.log(id)
     const blogitem = BLOGDATAS.find(blogitem => String(blogitem.id) === id);
+
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+      const foundData =  BLOGDATAS.find(blogitem => String(blogitem.id) === id);
+      if (foundData) {
+        setData(foundData);
+      } else {
+        navigate('/error');
+      }
+    }, [id, navigate]);
+  
+  //   return data ? <div>{data.content}</div> : null;
+  // }
  
-    return(
+  return data ? (
+ 
+    // return(
     <main>
     {/* <img src={`/${blogitem.image}`} alt={blogitem.alt} />
     {blogdataitem.map((blogitemText, id) =>(
@@ -36,7 +73,7 @@ const BlogItem =(props) =>{
                 </div>
 
     </main>
-    )
+    ) : null
 }
 
 export default BlogItem
