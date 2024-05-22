b3a35de4aee368a054522b231baad33e6bca8f13
meteoben <meteoben@gmail.com>
2024-05-22 17:41:56 +0200

    implémentation carousel contact pas possible de régler le height en 16/9 pour le moment

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,8 +1,31 @@
+import FullCarousel from "../../components/carousel/FullCarousel.jsx";
+import "./contact.scss";
 const Contact = () => {
+    
+    const ImageContactData = [
+    
+        {
+          image:
+            "images/entree.webp",
+        },
+        {
+          image:
+            "images/salle-attente.webp",
+        },
+   
+      ];  
     return (
         <main>
             <div className="banner-container">
                 <img src="images/header-contact.webp" alt="contact" />
+                </div>
+                <div className="contact-info">
+                    <h2>ANNE CHRISTINE BRUNEL</h2>
+                    <p>Diététicienne et micronutritionniste</p>
+                    <p>06 12 34 56 78</p>
+                      </div> 
+                      <div className="full-carousel-container">
+                <FullCarousel bigSlides={ImageContactData} className="carousel-image"  />
                 </div>
         <h1>Contact</h1>
         </main>
