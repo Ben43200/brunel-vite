bd75492b8cbf85c317cbc62c0b37d62e5454822e
meteoben <meteoben@gmail.com>
2024-05-23 15:36:14 +0200

    implémentation Reactlazy FullCarousel

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,38 +1,43 @@
-import FullCarousel from "../../components/carousel/FullCarousel.jsx";
-import "./contact.scss";
-import { useEffect,Suspense } from "react";
+// nouveau code
+import React , { useState, useEffect, Suspense } from 'react';
+import './contact.scss';
+
+const FullCarousel = React.lazy(() => import('../../components/carousel/FullCarousel.jsx'));
+
 const Contact = () => {
-    
-    const ImageContactData = [
-    
-     
-            "images/entree.webp",
-     
-            "images/salle-attente.webp",
-   
-      ];
-      useEffect(() => {
-        ImageContactData.forEach((image) => {
-          const img = new Image();
-          img.src = image;
-        });
-      }, []);  
-    return (
-        <main className="main-contact-container">
-            <div className="banner-container">
-                <img src="images/header-contact.webp" alt="contact" />
-                </div>
-                <div className="contact-info">
-                    <h2>ANNE CHRISTINE BRUNEL</h2>
-                    <p>Diététicienne et micronutritionniste</p>
-                      </div> 
-                      <div className="full-carousel-container">
-                      <Suspense fallback={<div>Chargement...</div>}>
-                <FullCarousel images={ImageContactData} className="carousel-image"  />
-                </Suspense>
-                </div>
-        <h1>Contact</h1>
-        </main>
-    );
-    };
-    export default Contact;
+  const [images, setImages] = useState([]);
+
+  const ImageContactData = [
+    "images/entree.webp",
+    "images/salle-attente.webp",
+  ];
+
+  
+  useEffect(() => {
+    ImageContactData.forEach((image) => {
+      const img = new Image();
+      img.src = image;
+      img.onload = () => setImages((prevImages) => [...prevImages, image]); // Ajoutez l'URL de l'image, pas l'objet Image
+    });
+  }, []);
+
+  return (
+    <main className="main-contact-container">
+      <div className="banner-container">
+        <img src="images/header-contact.webp" alt="contact" />
+      </div>
+      <div className="contact-info">
+        <h2>ANNE CHRISTINE BRUNEL</h2>
+        <p>Diététicienne et micronutritionniste</p>
+      </div> 
+      <div className="full-carousel-container">
+        <Suspense fallback={<div>Chargement...</div>}>
+          <FullCarousel images={images} className="carousel-image"  />
+        </Suspense>
+      </div>
+      <h1>Contact</h1>
+    </main>
+  );
+};
+
+export default Contact;
