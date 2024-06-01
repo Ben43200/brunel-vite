48a86fa1cc7a0aa53d43b12ebeba23960071e37c
meteoben <meteoben@gmail.com>
2024-06-01 05:57:21 +0200

    logique checkbox OpenMapComponent ok ok

--- src/components/mapcomponent/OpenMapComponent.jsx
+++ src/components/mapcomponent/OpenMapComponent.jsx
@@ -1,21 +1,51 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import { useState, useEffect } from 'react';
+
+const SetInteraction = ({ isInteractive }) => {
+  const map = useMap();
+
+  useEffect(() => {
+    if (isInteractive) {
+      map.dragging.enable();
+      map.touchZoom.enable();
+      map.doubleClickZoom.enable();
+      map.scrollWheelZoom.enable();
+    } else {
+      map.dragging.disable();
+      map.touchZoom.disable();
+      map.doubleClickZoom.disable();
+      map.scrollWheelZoom.disable();
+    }
+  }, [isInteractive, map]);
+
+  return null;
+}
+
 const OpenMapComponent = () => {
   const position = [44.72473366745566, 5.032174801844155]; // Position par défaut, vous pouvez la changer
+  const [isInteractive, setIsInteractive] = useState(false);
 
   return (
-    <MapContainer center={position} zoom={13} style={{ height: "100vh", width: "100%" }}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    <div>
+      <input 
+        type="checkbox" 
+        checked={isInteractive} 
+        onChange={() => setIsInteractive(!isInteractive)} 
       />
-      <Marker position={position}>
-        <Popup>
-          Un popup exemple.
-        </Popup>
-      </Marker>
-    </MapContainer>
+      <MapContainer center={position} zoom={13} style={{ height: "100vh", width: "100%" }}>
+        <SetInteraction isInteractive={isInteractive} />
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={position}>
+          <Popup>
+            Un popup exemple.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
   );
 }
 
 export default OpenMapComponent;
-
