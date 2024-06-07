1f29a1d8a0b419dea1cea238932a260fd5ebfcc7
meteoben <meteoben@gmail.com>
2024-06-07 13:34:22 +0200

    essai react lazy fullcarousel

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,10 @@
 import "./home.scss";
-import FullCarousel from "../../components/carousel/FullCarousel.jsx";
-import  { useEffect } from "react";
+// import FullCarousel from "../../components/carousel/FullCarousel.jsx";
+import  { useEffect, Suspense } from "react";
 // import OpenMapComponent from "../../components/mapcomponent/OpenMapComponent.jsx";
 // import 'leaflet/dist/leaflet.css';
 
-// const FullCarousel = React.lazy(() => import('../../components/carousel/FullCarousel.jsx'));
+const FullCarousel = React.lazy(() => import('../../components/carousel/FullCarousel.jsx'));
 const Home = () => {
   const ImageData = [
     "images/accueil1.jpg",
@@ -24,10 +24,10 @@ const Home = () => {
   return (
     <main className="home-container">
       <div className="carousel-home-container">
-      {/* <Suspense fallback={<div>Chargement...</div>}> */}
+      <Suspense fallback={<div>Chargement...</div>}>
         <FullCarousel images={ImageData} />
         </div>
-        {/* </Suspense> */}
+        </Suspense>
     {/* {/* <OpenMapComponent />  */}
 
     </main>
