76b4619085f13eb3578950f5967e6e5e67480bee
meteoben <meteoben@gmail.com>
2024-06-10 18:03:33 +0200

    suppression commentaire trop long passage grid-template-rows 3fr 1fr home-container ok

--- src/components/TestimonialCarousel.jsx
+++ src/components/TestimonialCarousel.jsx
@@ -3,16 +3,12 @@ import './testimonialcarousel.scss';
 
 const testimonials = [
   { name: "Clémentine", testimonial: " Madame Brunel a enfin pu répondre à mes besoins de soins après des mois d’errance. Le diagnostic de candidose posé, sa compréhension et connaissance de la maladie a permis un accompagnement de grande qualité et une disponibilité rare. Un grand merci ! " },
-  { name: "Alice", testimonial: " Malade depuis plus de 3 ans et en errance médicale, Mme BRUNEL à défini un possible diagnostic qui s'est avéré exact. Elle m'a permis au bout de 6 mois de reprendre mon travail. Le chemin est encore long mais, elle est un réel soutien. Elle est disponible, bienveillante et pointue concernant ma pathologie, toujours dans une démarche de formation et de recherche pour adapter au mieux ses protocoles de soin." +
-	"" +
-	"Encore mille mercis, elle m'a sauvé d'un avenir qui m'aurait amené à de l'invalidité." +
-	"" +
-	"Je la conseille à mille pour cent !! "},
+  { name: "Christiane", testimonial: " Anne-Christine est une personne non seulement compétente mais aussi à l'écoute avec bienveillance et empathie. Les compléments alimentaires qu’elle me prescrit, sont choisis avec soin et correspondent à ma problématique. Ils m'apportent à chaque fois plus de bien-être et une meilleure santé "},
   { name: "Nassera", testimonial: " Madame BRUNEL est une excellente diététicienne micronutritioniste, très professionnelle, à l'écoute, disponible et de très bons conseils car se forme et s'informe continuellement." +
 	"" +
 	"Vous serez toujours reçu avec gentillesse et bienveillance." +
 	"" +
-	"MERCI pour tout Madame BRUNEL "},
+	"MERCI pour tout Madame BRUNEL"},
 ];
 
 function TestimonialCarousel() {
