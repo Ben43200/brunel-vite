edaab50e3a941eb2e80b3c55231a997fdf180aef
meteoben <meteoben@gmail.com>
2024-06-10 16:19:44 +0200

    modif footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,10 @@ export default function Footer(){
 
     return (
         <footer>
-            <h4>© Footer</h4>
+            <div className="footer-container">
+            <p>©Meteoben</p>
+            <p>Mentions Légales</p>
+            </div>
         </footer>
     )
 }
