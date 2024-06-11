e26c1818dde08438b67b95ac0ec5d23f7b9dc4db
meteoben <meteoben@gmail.com>
2024-06-11 10:09:07 +0200

    ajout useeffect préchargement imagedata desktop et mobile

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,36 +1,37 @@
 import "./home.scss";
 import FullCarousel from "../../components/carousel/FullCarousel.jsx";
-import  { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import TestimonialCarousel from "../../components/TestimonialCarousel.jsx";
-
 
 // const FullCarousel = React.lazy(() => import('../../components/carousel/FullCarousel.jsx'));
 const Home = () => {
-
   const ImageDataDesktop = [
     "images/ACB-ORDI-1.jpeg",
     "images/ACB-ORDI-2.jpeg",
     "images/ACB-ORDI-3.jpeg",
-  ]
-  const ImageDataMobile=[
+  ];
+  const ImageDataMobile = [
     "images/ACB-MOBILE-1.jpg",
     "images/ACB-MOBILE-2.jpg",
     "images/ACB-MOBILE-3.jpg",
-  ]
+  ];
 
-
-  const [isDesktopOrLaptop, setIsDesktopOrLaptop] = useState(window.innerWidth >=600);
-  const [ImageData, setImageData] = useState(isDesktopOrLaptop ? ImageDataDesktop : ImageDataMobile);
+  const [isDesktopOrLaptop, setIsDesktopOrLaptop] = useState(
+    window.innerWidth >= 600
+  );
+  const [ImageData, setImageData] = useState(
+    isDesktopOrLaptop ? ImageDataDesktop : ImageDataMobile
+  );
 
   useEffect(() => {
     const handleResize = () => {
       setIsDesktopOrLaptop(window.innerWidth >= 600);
     };
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
@@ -38,23 +39,29 @@ const Home = () => {
     setImageData(isDesktopOrLaptop ? ImageDataDesktop : ImageDataMobile);
   }, [isDesktopOrLaptop]);
 
+  // useEffect(() => {
+  //   ImageData.forEach((image) => {
+  //     const img = new Image();
+  //     img.src = image;
+  //   });
+  // }, [ImageData]);
   useEffect(() => {
-    ImageData.forEach((image) => {
+    // Précharge toutes les images pour les versions desktop et mobile
+    [...ImageDataDesktop, ...ImageDataMobile].forEach((image) => {
       const img = new Image();
       img.src = image;
     });
-  }, [ImageData]);
+  }, []); // Passer un tableau vide en dépendance pour s'assurer que cela ne s'exécute qu'au montage du composant
 
   return (
     <main className="home-container">
       <div className="carousel-home-container">
-      {/* <Suspense fallback={<div>Chargement...</div>}> */}
+        {/* <Suspense fallback={<div>Chargement...</div>}> */}
         <FullCarousel images={ImageData} />
         {/* </Suspense> */}
-        </div>
+      </div>
 
-
-<TestimonialCarousel />
+      <TestimonialCarousel />
     </main>
   );
 };
