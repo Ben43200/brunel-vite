ef3d8d20c8c6c1686e5c865962557ae3a92a487e
meteoben <meteoben@gmail.com>
2024-06-11 16:01:09 +0200

    ajout jusqu'à dysbiose ok plus ajout tclass first-content

--- src/assets/BlogDataItem.jsx
+++ src/assets/BlogDataItem.jsx
@@ -31,53 +31,162 @@
 
 export const blogdataitem = [
   {
-  id:0,
-  content: [
-  <p key={0}>
-    On en parle beaucoup, mais que sont-ils exactement ? A quoi servent-ils ? Où
-    les trouve-t-on ? Les omégas 3 sont des acides gras à longues chaines avec
-    plusieurs doubles liaisons. Ce sont des acides gras polyinsaturés. Le
-    précurseur en est l’acide alpha linolénique : il se transforme grâce à
-    différentes enzymes en EPA et DHA, qui sont les omégas 3. Nos membranes
-    cellulaires sont formées de deux couches d’acides gras. Si nous manquons
-    d’omégas 3, elles seront plus rigides, alors notre réponse inflammatoire
-    lors d’attaques virales, bactériennes… sera exagérée.
-  </p>,
-  <p key={1}>
-    C’est pourquoi les omégas 3 sont si importants pour nous protéger des
-    problèmes cardiovasculaires. Ils ont un rôle « antiinflammatoire ». Ils sont
-    prioritaires pour nos yeux, notre peau, et notre cerveau….
-  </p>,
-  <p key={2}>
-    L’acide alpha linolénique se trouve dans les végétaux : colza, lin,
-    caméline, noix, mâche, roquette, pourpier, jeunes pousses d’épinard, algues.
-  </p>,
-  <p key={3}>
-    L’EPA et DHA se trouvent dans les poissons gras : sardines, maquereaux,
-    saumon, thon, algues et dans les produits provenant de la filière
-    bleu-blanc-cœur (Cf. «Au petit déjeuner, on nourrit ses neurones »).
-  </p>,
-  <p key={4}>
-    Dans beaucoup de situations (enfant de moins de 18 mois, adulte de plus de
-    70 ans, dysbiose, stress, insulinorésistance, diabète, prises de certains
-    médicaments…), une des enzymes qui transforme l’acide alpha linolénique en
-    omégas 3 ne fonctionne pas bien. C’est pourquoi il est important d’apporter
-    à notre corps chaque jour des sources végétales du précurseur, et
-    régulièrement des sources animales d’omégas3.
-  </p>
-]},
-{
-id:2,
-content:
-[<h3 key={0}>Micronutrition, une approche de la nutrition médicalisée adaptée à chacun...</h3>,
-  <p key={1}>La Micronutrition consiste à satisfaire les besoins en micronutriments de l’individu, par une alimentation diversifiée, associée si nécessaire à une complémentation personnalisée. Elle trouve ses fondements dans les recherches effectuées sur les liens avérés entre alimentation, santé et prévention.</p>,
-  <h3 key={2}>Pourquoi la Micronutrition plutôt que la nutrithérapie ?</h3>,
-  <p key={3}>La Micronutrition s’intéresse à l’impact des micronutriments sur la santé et vise à optimiser le statut en micronutriments de l’organisme de manière individualisée. Elle choisit donc de regarder les précieuses molécules qui se trouvent dans notre assiette et la façon dont notre organisme les utilise. Elle prend toute sa dimension aujourd’hui avec une alimentation qui se caractérise par l’augmentation de l’apport calorique au détriment de la densité en micronutriments. C’est ce qu’on appelle d’ailleurs « les calories vides ».</p>,
-  <h3 key={4}>Les MICROnutriments : des composés MEGAimportants</h3>,
-  <p key={5}>Schématiquement, on retrouve dans nos aliments :</p>,
-  <p key={6}>-<strong>des macronutriments</strong> qui fournissent l’énergie et la force nécessaires à l’organisme. Ce sont les lipides, les glucides et les protéines.</p>,
-  <p key={7}>-<strong>des micronutriments</strong> qui ne jouent aucun rôle énergétique mais dont le rôle est fondamental pour le fonctionnement de l’ensemble des métabolismes. Ce sont les vitamines, minéraux, oligo-éléments, acides gras essentiels mais aussi les flavonoïdes, acides aminés, probiotiques ...</p>,
-  <p key={9}>Tous ces éléments donnent la valeur nutritionnelle de l’assiette.</p>
-]
-},
+    id: 0,
+    content: [
+      <p className="first-content" key={0}>
+        On en parle beaucoup, mais que sont-ils exactement ? A quoi servent-ils
+        ? Où les trouve-t-on ? Les omégas 3 sont des acides gras à longues
+        chaines avec plusieurs doubles liaisons. Ce sont des acides gras
+        polyinsaturés. Le précurseur en est l’acide alpha linolénique : il se
+        transforme grâce à différentes enzymes en EPA et DHA, qui sont les
+        omégas 3. Nos membranes cellulaires sont formées de deux couches
+        d’acides gras. Si nous manquons d’omégas 3, elles seront plus rigides,
+        alors notre réponse inflammatoire lors d’attaques virales, bactériennes…
+        sera exagérée.
+      </p>,
+      <p key={1}>
+        C’est pourquoi les omégas 3 sont si importants pour nous protéger des
+        problèmes cardiovasculaires. Ils ont un rôle « antiinflammatoire ». Ils
+        sont prioritaires pour nos yeux, notre peau, et notre cerveau….
+      </p>,
+      <p key={2}>
+        L’acide alpha linolénique se trouve dans les végétaux : colza, lin,
+        caméline, noix, mâche, roquette, pourpier, jeunes pousses d’épinard,
+        algues.
+      </p>,
+      <p key={3}>
+        L’EPA et DHA se trouvent dans les poissons gras : sardines, maquereaux,
+        saumon, thon, algues et dans les produits provenant de la filière
+        bleu-blanc-cœur (Cf. «Au petit déjeuner, on nourrit ses neurones »).
+      </p>,
+      <p key={4}>
+        Dans beaucoup de situations (enfant de moins de 18 mois, adulte de plus
+        de 70 ans, dysbiose, stress, insulinorésistance, diabète, prises de
+        certains médicaments…), une des enzymes qui transforme l’acide alpha
+        linolénique en omégas 3 ne fonctionne pas bien. C’est pourquoi il est
+        important d’apporter à notre corps chaque jour des sources végétales du
+        précurseur, et régulièrement des sources animales d’omégas3.
+      </p>,
+    ],
+  },
+  {
+    id: 2,
+    content: [
+      <h3 className="first-content" key={0}>
+        Micronutrition, une approche de la nutrition médicalisée adaptée à
+        chacun...
+      </h3>,
+      <p key={1}>
+        La Micronutrition consiste à satisfaire les besoins en micronutriments
+        de l’individu, par une alimentation diversifiée, associée si nécessaire
+        à une complémentation personnalisée. Elle trouve ses fondements dans les
+        recherches effectuées sur les liens avérés entre alimentation, santé et
+        prévention.
+      </p>,
+      <h3 key={2}>Pourquoi la Micronutrition plutôt que la nutrithérapie ?</h3>,
+      <p key={3}>
+        La Micronutrition s’intéresse à l’impact des micronutriments sur la
+        santé et vise à optimiser le statut en micronutriments de l’organisme de
+        manière individualisée. Elle choisit donc de regarder les précieuses
+        molécules qui se trouvent dans notre assiette et la façon dont notre
+        organisme les utilise. Elle prend toute sa dimension aujourd’hui avec
+        une alimentation qui se caractérise par l’augmentation de l’apport
+        calorique au détriment de la densité en micronutriments. C’est ce qu’on
+        appelle d’ailleurs « les calories vides ».
+      </p>,
+      <h3 key={4}>Les MICROnutriments : des composés MEGAimportants</h3>,
+      <p key={5}>Schématiquement, on retrouve dans nos aliments :</p>,
+      <p key={6}>
+        -<strong>des macronutriments</strong> qui fournissent l’énergie et la
+        force nécessaires à l’organisme. Ce sont les lipides, les glucides et
+        les protéines.
+      </p>,
+      <p key={7}>
+        -<strong>des micronutriments</strong> qui ne jouent aucun rôle
+        énergétique mais dont le rôle est fondamental pour le fonctionnement de
+        l’ensemble des métabolismes. Ce sont les vitamines, minéraux,
+        oligo-éléments, acides gras essentiels mais aussi les flavonoïdes,
+        acides aminés, probiotiques ...
+      </p>,
+      <p key={9}>
+        Tous ces éléments donnent la valeur nutritionnelle de l’assiette.
+      </p>,
+    ],
+  },
+  {
+    id: 3,
+    content: [
+      <p className="first-content" key={0}>
+        Parce qu’elle n’est pas réservée à une population particulière mais au
+        contraire adaptée pour apporter des solutions personnalisées à chaque
+        période de la vie, la micronutrition s’adresse à tous ceux qui :
+      </p>,
+      <p key={1}>
+        • souffrent d’inconfort digestif, du syndrome du colon irritable, de
+        colite, de troubles de la détoxication,
+      </p>,
+      <p key={2}>
+        • rencontrent des problèmes de concentration, de mémoire, de sommeil,
+      </p>,
+      <p key={3}>
+        • se plaignent de douleurs musculaires, articulaires ou rhumatismales,
+      </p>,
+      <p key={4}>
+        • sont particulièrement sensibles aux refroidissements hivernaux, aux
+        allergies,
+      </p>,
+      <p key={5}>• se sentent fatigués, déprimés,</p>,
+      <p key={6}>
+        • rencontrent des désordres hormonaux (cycles irréguliers, ménopause,
+        troubles de la prostate),
+      </p>,
+      <p key={7}>• souffrent de syndrome métabolique, diabète, obésité,</p>,
+      <p key={8}>
+        • souhaitent affiner leur silhouette tout en restant en forme et en
+        bonne santé,
+      </p>,
+      <p key={9}>• désirent optimiser leurs performances sportives,</p>,
+      <p key={10}>
+        • souhaitent une alimentation équilibrée durant leur grossesse pour leur
+        bien-être et celui du bébé,
+      </p>,
+      <p key={11}>
+        • souhaitent favoriser la croissance physique, intellectuelle et
+        émotionnelle de leurs enfants et adolescents,
+      </p>,
+      <p key={12}>
+        • souhaitent vivre en forme pleinement et le plus longtemps possible…
+      </p>,
+      <p key={13}>
+        … quelques situations, entre-autres, pour lesquelles{" "}
+        <strong>
+          micronutrition s’avère efficace et sans effets secondaires
+          désagréables.
+        </strong>
+      </p>,
+    ],
+  },
+  {
+    id: 4,
+    content: [
+      <p key={0} className="first-content">
+        La dysbiose (par opposition à l’eubiose) est le nom donné à « la guerre
+        dans l’intestin ». Les bactéries « méchantes » et autres microorganismes
+        (tels que levures, parasites) ont pris le dessus sur nos bactéries
+        amies. Cette dysbiose est certainement à l’origine de toutes maladies
+        chroniques (syndrome métabolique, diabète, obésité, problèmes
+        cardiovasculaires, maladies auto-immunes, allergies, fatigue chronique,
+        dépression, fibromyalgie, problème de peau…). La « mal bouffe », le
+        stress, la prise de médicaments sont souvent des facteurs responsables
+        ou déclenchants. C’est pourquoi, il faut veiller à bien nourrir le
+        microbiote avec une alimentation saine, la moins transformée possible
+        (partir de l’aliment brut et le cuisiner soi-même), très variée, riche
+        en micronutriments (avec légumes et fruits de saison bio), la moins
+        raffinée possible, et réapprendre à <strong>bien mastiquer</strong> pour permettre une
+        bonne digestion. Une éviction alimentaire sera nécessaire dans les cas
+        d’hypersensibilité à certains aliments (gluten, protéines de lait de
+        vache, lactose, œufs, Fodmap…).
+      </p>,
+    ],
+  },
 ];
