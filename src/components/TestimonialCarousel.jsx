9305f49729c6cfb7c533b27b0ff0c170be27725a
meteoben <meteoben@gmail.com>
2024-06-17 16:18:54 +0200

    simplification commentaire Alice

--- src/components/TestimonialCarousel.jsx
+++ src/components/TestimonialCarousel.jsx
@@ -2,6 +2,7 @@ import  { useState, useEffect } from 'react';
 import './testimonialcarousel.scss';
 
 const testimonials = [
+  {name: "Alice", testimonial: "Malade depuis plus de 3 ans et en errance médicale, Mme BRUNEL a défini un possible diagnostic qui s'est avéré exact. Elle m'a permis au bout de 6 mois de reprendre mon travail. Le chemin est encore long mais, elle est un réel soutien. Elle est disponible, bienveillante et pointue concernant ma pathologie. Mille mercis, elle m'a sauvé d'un avenir qui m'aurait amené à de l'invalidité. Je la conseille à mille pour cent !!"},
   { name: "Clémentine", testimonial: " Madame Brunel a enfin pu répondre à mes besoins de soins après des mois d’errance. Le diagnostic de candidose posé, sa compréhension et connaissance de la maladie a permis un accompagnement de grande qualité et une disponibilité rare. Un grand merci !" },
   { name: "Christiane", testimonial: " Anne-Christine est une personne non seulement compétente mais aussi à l'écoute avec bienveillance et empathie. Les compléments alimentaires qu’elle me prescrit, sont choisis avec soin et correspondent à ma problématique. Ils m'apportent à chaque fois plus de bien-être et une meilleure santé. "},
   { name: "Nassera", testimonial: " Madame BRUNEL est une excellente diététicienne micronutritioniste, très professionnelle, à l'écoute, disponible et de très bons conseils car se forme et s'informe continuellement." +
@@ -18,11 +19,6 @@ const testimonials = [
 	"Elle saura vous accompagner dans vos problématiques avec volonté et persévérance pour vous trouver une solution. Et ainsi rendre votre vie plus agréable. Je ne peux que vous la recommander 🙂"},
 {name: "Robert", testimonial: " L'accompagnement de Madame Brunel est précieux car rien n'est laissé au hasard. Je peux me reposer en toute confiance sur son diagnostic étant donnés les résultats obtenus"},
 {name: "Lamiel", testimonial: "Merci à Anne-Christine pour son professionnalisme "},
-{name: "Alice", testimonial: "Malade depuis plus de 3 ans et en errance médicale, Mme BRUNEL à défini un possible diagnostic qui s'est avéré exact. Elle m'a permis au bout de 6 mois de reprendre mon travail. Le chemin est encore long mais, elle est un réel soutien. Elle est disponible, bienveillante et pointue concernant ma pathologie, toujours dans une démarche de formation et de recherche pour adapter au mieux ses protocoles de soin." +
-	"" +
-	"Encore mille mercis, elle m'a sauvé d'un avenir qui m'aurait amené à de l'invalidité." +
-	"" +
-	"Je la conseille à mille pour cent !!"},
 {name: "Nicole", testimonial: "Je suis suivie par Madame Brunel en diététique depuis 2 ans, elle a une très bonne écoute, je reconnais son professionnalisme et son sérieux, elle a su pallier à mes problèmes de santé, elle accompagne aussi mes enfants." +
 	"" +
 	"Je suis satisfaite de sa prestation et je la recommande"},
