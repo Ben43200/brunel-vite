4783fed363a77f739d905bbf129514f2b2274ec4
meteoben <meteoben@gmail.com>
2024-06-17 13:19:51 +0200

    mise à l'échelle image-prestation

--- src/pages/prestations/Prestations.jsx
+++ src/pages/prestations/Prestations.jsx
@@ -4,13 +4,6 @@ const Prestations = () => {
   return (
     <main className="prestations-container">
       <h2 className="prestations-title">Prestations</h2>
-      <div className="prestations-img-container">
-        <img
-          src="images/itinerary.png"
-          alt="puzzle"
-          className="prestations-image"
-        />
-      </div>
       <h3 className="prestations-subtitle">COMMENT SE PASSE UNE PREMIÈRE CONSULTATION ?</h3>
       <div className="prestations-img-container">
         <img
@@ -19,6 +12,14 @@ const Prestations = () => {
           className="prestations-image"
         />
       </div>
+      <div className="prestations-img-container">
+        <img
+          src="images/itinerary.png"
+          alt="puzzle"
+          className="prestations-image"
+        />
+      </div>
+
     </main>
   );
 }
