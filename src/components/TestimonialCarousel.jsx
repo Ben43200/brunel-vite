7ee75c3d1fc5b537f9926578f027eb54b9a72297
meteoben <meteoben@gmail.com>
2024-06-17 07:25:11 +0200

    passage testimonia authorl même taille et même ligne

--- src/components/TestimonialCarousel.jsx
+++ src/components/TestimonialCarousel.jsx
@@ -2,13 +2,13 @@ import  { useState, useEffect } from 'react';
 import './testimonialcarousel.scss';
 
 const testimonials = [
-  { name: "Clémentine", testimonial: " Madame Brunel a enfin pu répondre à mes besoins de soins après des mois d’errance. Le diagnostic de candidose posé, sa compréhension et connaissance de la maladie a permis un accompagnement de grande qualité et une disponibilité rare. Un grand merci ! " },
-  { name: "Christiane", testimonial: " Anne-Christine est une personne non seulement compétente mais aussi à l'écoute avec bienveillance et empathie. Les compléments alimentaires qu’elle me prescrit, sont choisis avec soin et correspondent à ma problématique. Ils m'apportent à chaque fois plus de bien-être et une meilleure santé "},
+  { name: "Clémentine", testimonial: " Madame Brunel a enfin pu répondre à mes besoins de soins après des mois d’errance. Le diagnostic de candidose posé, sa compréhension et connaissance de la maladie a permis un accompagnement de grande qualité et une disponibilité rare. Un grand merci !" },
+  { name: "Christiane", testimonial: " Anne-Christine est une personne non seulement compétente mais aussi à l'écoute avec bienveillance et empathie. Les compléments alimentaires qu’elle me prescrit, sont choisis avec soin et correspondent à ma problématique. Ils m'apportent à chaque fois plus de bien-être et une meilleure santé. "},
   { name: "Nassera", testimonial: " Madame BRUNEL est une excellente diététicienne micronutritioniste, très professionnelle, à l'écoute, disponible et de très bons conseils car se forme et s'informe continuellement." +
 	"" +
 	"Vous serez toujours reçu avec gentillesse et bienveillance." +
 	"" +
-	"MERCI pour tout Madame BRUNEL"},
+	"MERCI pour tout Madame BRUNEL."},
 ];
 
 function TestimonialCarousel() {
@@ -36,8 +36,8 @@ function TestimonialCarousel() {
           key={index}
           className={`carousel__slide ${index === current ? 'carousel__slide--active' : ''}`}
         >
-          <p className="testimonial">❝{testimonial.testimonial}❞</p>
-          <h2 className="testimonial-author">{testimonial.name}</h2>
+          <p className="testimonial">❝{testimonial.testimonial}❞ {testimonial.name}</p>
+          {/* <h2 className="testimonial-author">{testimonial.name}</h2> */}
         </div>
       ))}
     </div>
